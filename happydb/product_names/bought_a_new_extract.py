56b5b1cb7d08106806d18f9e125d0ff3ddaaf01c
Chen Chen <chen@recruit.ai>
2017-09-15 14:05:04 -0700

    Try to extract products preceded by 'bought a new' phrase

---
+++ happydb/product_names/bought_a_new_extract.py
@@ -0,0 +1,24 @@
+# Entity Extraction from HappyDB data
+# This code needs to be reviewed and rewritten
+
+import sys
+import argparse
+
+import pandas as pds
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-i", "--ipath", type=str, help="The path of the input file", default="/Users/chen/Research/Playground/Github_Playground/happydb/data/cleaned_hm.csv")
+parser.add_argument("-o", "--opath", type=str, help="The path of the output file", default="/Users/chen/default_purchase.txt")
+args = parser.parse_args()
+
+# Read the happyDB sample file
+input_file = args.ipath
+data = pds.read_csv(input_file)
+data_sub = data
+
+# Find all lines containing "buy", "purchase" or "bought" etc.
+output_file = args.opath
+with open(output_file, 'w') as ofile:
+    for i in range(0, data_sub['cleaned_hm'].size-1):
+        if 'bought a new' in data_sub['cleaned_hm'].iloc[i]:
+           ofile.write(data_sub['cleaned_hm'].iloc[i] + '\n')
