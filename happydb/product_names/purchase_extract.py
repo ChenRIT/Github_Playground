5e6d23883cd12e48ea366837d5711a4374fafacf
Chen Chen <chen@recruit.ai>
2017-09-22 13:56:59 -0700

    Update the purchasing extraction script

--- happydb/product_names/purchase_extract.py
+++ happydb/product_names/purchase_extract.py
@@ -7,8 +7,8 @@ import argparse
 import pandas as pds
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--ipath", type=str, help="The path of the input file", default="/Users/chen/default_hm.csv")
-parser.add_argument("--opath", type=str, help="The path of the output file", default="/Users/chen/default_purchase.txt")
+parser.add_argument("-i", "--ipath", type=str, help="The path of the input file", default="/Users/chen/Research/Playground/Github_Playground/happydb/data/cleaned_hm.csv")
+parser.add_argument("-o", "--opath", type=str, help="The path of the output file", default="/Users/chen/default_purchase.txt")
 args = parser.parse_args()
 
 # Read the happyDB sample file
@@ -23,5 +23,5 @@ with open(output_file, 'w') as ofile:
         if 'buy' in data_sub['cleaned_hm'].iloc[i] or \
            'bought' in data_sub['cleaned_hm'].iloc[i] or \
            'purchase' in data_sub['cleaned_hm'].iloc[i] or \
-           'order' in data_sub['cleaned_hm'].iloc[i]:
+           'shopping' in data_sub['cleaned_hm'].iloc[i]:
            ofile.write(data_sub['cleaned_hm'].iloc[i] + '\n')
