f3532dcc5f3d8b5eb8ff61e7c3b3d84177fa05f9
Chen Chen <chen@recruit.ai>
2018-02-08 11:38:31 -0800

    Use questions of cost to get training data of more variety

--- lbox/number_extractor/scripts/extract_qapair/extract_attr_qapair.py
+++ lbox/number_extractor/scripts/extract_qapair/extract_attr_qapair.py
@@ -6,12 +6,13 @@ from os.path import isfile, join
 import re
 import argparse
 import spacy
+from spacy.symbols import NUM
 
 nlp = spacy.load('en_core_web_sm')
 
 #questions = ['(?:how much does it cost)', '(?:what is the price)']
 
-def extract_sents(input_texts, output_file, post_pad_num=200):
+def extract_sents(input_texts, output_file, post_pad_num=100):
     """ Extract the sentences containing question answer pairs
 
     @return: the number of instances found
@@ -19,25 +20,40 @@ def extract_sents(input_texts, output_file, post_pad_num=200):
 
     ins_count = 0
     #reg_exp = "(?:" + '|'.join(questions) + ")" + ".{" + str(post_pad_num) + "}"
-    reg_exp = r"cost.{,10}\?.{" + str(post_pad_num) + "}"
+    reg_exp = r"cost.{,20}\?.{" + str(post_pad_num) + "}"
     print("Search for patterns: " + reg_exp)
 
     results = re.findall(reg_exp, input_texts, re.S|re.I)
     print("Find {} instances".format(len(results)))
 
-    for res in results:
-        print(res)
     # for res in results:
-    #     doc = nlp(res)
-    #     for sent in doc.sents:
-    #         search_salary = re.search(attr, sent.text, re.I)
-    #         if search_salary:
-    #             search_num = re.search(r"[0-9.,]+[0-9]", sent.text)
-    #             if search_num:
-    #                 ins_count += 1
-    #                 new_sent = sent.text.replace(attr, "ATTR")
-    #                 output_file.write(new_sent + "\n")
-    #             break
+    #     print(res)
+
+    for res in results:
+        doc = nlp(res)
+        sents = list(doc.sents)
+        for i in range(len(sents)):
+            if 'cost' in sents[i].text and \
+               '?' in sents[i].text and \
+               i+1 < len(sents):
+                next_sent = sents[i+1]
+                #search_num = re.search(r"[0-9.,]+[0-9]", next_sent.text)
+                # if search_num:
+                if "$" in next_sent.text or "dollar" in next_sent.text:
+                    has_num = False
+                    for token in next_sent:
+                        if token.pos == NUM:
+                            has_num = True
+                            break
+                    if not has_num:
+                        break
+                    
+                    ins_count += 1
+                    # print("Question: " + sents[i].text + "\n")
+                    # print("Answer: " + next_sent.text + "\n\n")                    
+                    # output_file.write("Question: " + sents[i] + "\n")
+                    output_file.write(next_sent.text + "\n")
+                break
 
     return ins_count
 
@@ -45,24 +61,27 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("-ifnames", nargs='+', help="The data files to be opened.", default=None)
     parser.add_argument("-dir", type=str, help="The directory to find input files.", default=None)    
-    parser.add_argument("-pr", "--padding_right", type=int, help="The number of characters to be extracted to the right of the searched pattern", default=200)
+    parser.add_argument("-pr", "--padding_right", type=int, help="The number of characters to be extracted to the right of the searched pattern", default=100)
     args = parser.parse_args()
 
     ifnames = args.ifnames
+    ofname = "price_results.txt"
     dir = args.dir
     pad_right = args.padding_right
     ins_count = 0
     all_files = []
-    if ifnames:
-        all_files += ifnames
-    if dir:
-        dir_files = [join(dir,f) for f in listdir(dir) if isfile(join(dir, f))]
-        all_files += dir_files
+    with open(ofname, "w") as ofile:
+        if ifnames:
+            all_files += ifnames
 
-    for fname in all_files:
-        if "warc.gz.txt" in fname:
-            with open(fname, "r", errors='replace') as ifile:
-                doc = ifile.read()
-                ins_count += extract_sents(doc, None, pad_right)
+        if dir:
+            dir_files = [join(dir,f) for f in listdir(dir) if isfile(join(dir, f))]
+            all_files += dir_files
+
+        for fname in all_files:
+            if "warc.gz.txt" in fname:
+                with open(fname, "r", errors='replace') as ifile:
+                    doc = ifile.read()
+                    ins_count += extract_sents(doc, ofile, pad_right)
             
     #print("Number of final entries: {}".format(ins_count))
