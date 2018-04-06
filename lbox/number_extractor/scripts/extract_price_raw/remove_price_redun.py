323058c42c8a15080db71c0496c83c6224f1c883
Chen Chen <chen@recruit.ai>
2018-04-06 10:21:18 -0700

    Add the script for redundancy removal

--- lbox/number_extractor/scripts/extract_price_raw/remove_price_redun.py
+++ lbox/number_extractor/scripts/extract_price_raw/remove_price_redun.py
@@ -13,19 +13,22 @@ def get_template(sent):
     return new_sent
 
 if __name__ == "__main__":
-    input_file = "./test_redundant_sents.txt"
-    output_file = "./test_redun_removed.txt"
+    input_dir = "./price_sents_snapshot"
+    output_file = "./sents_no_redun.txt"
 
     template_pool = []
     non_redun_sents = []
-    with open(input_file) as ifile:
-        for line in ifile:
-            sent_template = get_template(line)
-            if sent_template in template_pool:
-                continue
-            else:
-                template_pool.append(sent_template)
-                non_redun_sents.append(line)
+    for fname in os.listdir(input_dir):
+        if fname.endswith(".warc.gz.txt"):
+            fpath = os.path.join(input_dir, fname)
+            with open(fpath) as ifile:
+                for line in ifile:
+                    sent_template = get_template(line)
+                    if sent_template in template_pool:
+                        continue
+                    else:
+                        template_pool.append(sent_template)
+                        non_redun_sents.append(line)
 
     with open(output_file, "w") as ofile:
         for sent in non_redun_sents:
